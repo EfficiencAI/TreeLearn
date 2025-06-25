69e0e8b147220884e20d2acd02301dbf0053373a
M1 <m1sakamikoto@foxmail.com>
2025-06-25 13:51:52 +0800

    改动： 添加了getAllConversationNodesId接口     api:/user/conversation/getAllConversationNodesId?userId={userId}&sessionName={sessionName}     返回值中returnValue的内容为由String类型(sessionName)组成的HashSet 添加了getAllSessionsName接口     api:/user/getAllSessionsName?userId={userId}     返回值中returnValue的内容为由String类型(conversationNodeID)组成的HashSet

--- src/main/java/io/github/EfficiencAI/pojo/VO/NodeOperationResult.java
+++ src/main/java/io/github/EfficiencAI/pojo/VO/NodeOperationResult.java
@@ -1,23 +1,20 @@
 package io.github.EfficiencAI.pojo.VO;
 
-import io.github.EfficiencAI.pojo.Entites.node.Base.PersistentNode;
-import io.github.EfficiencAI.pojo.Entites.node.SessionNode;
-
-public class NodeOperationResult<T extends PersistentNode> {
+public class NodeOperationResult<T> {
     public enum OperationType {
         CREATE,
         GET,
         MODIFY,
         DELETE
     }
-    public NodeOperationResult(OperationType operationType, T node, boolean ifSuccess, String note) {
+    public NodeOperationResult(OperationType operationType, T returnValue, boolean ifSuccess, String note) {
         this.operationType = operationType;
-        this.node = node;
+        this.returnValue = returnValue;
         this.ifSuccess = ifSuccess;
         this.note = note;
     }
     public OperationType operationType = OperationType.CREATE;
-    public T node = null;
+    public T returnValue = null;
     public boolean ifSuccess = false;
     public String note = "";
 }
